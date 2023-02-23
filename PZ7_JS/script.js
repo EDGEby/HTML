87bf70fe8c30044af160aa8f572eb78c77587aee
Daniil Laman <edgesea@yandex.by>
2023-02-23 20:54:26 +0300

    Add files via upload

---
+++ PZ7_JS/script.js
@@ -0,0 +1,89 @@
+/*function task3(num1,num2,num3){
+	return(num1+num2+num3)/3;
+}
+
+let a =1;
+let b =3;
+let c =5;
+console.log(task3(a,b,c));
+
+*/
+/*function allEven(mas){
+	for(let i=0;i<mas.length;i++){
+		if( mas[i]%2!=0){
+			return false;
+		}
+		
+	}
+	return true;
+}
+
+let mas=[];
+for(let i=0; i<5;i++){
+	let x = Math.floor(Math.random()*11);
+	mas.push(x);
+}
+if (allEven(mas)){
+	console.log(mas);
+	console.log("true");
+}
+else{
+	console.log(mas);
+	console.log("false");
+}
+
+*/
+/*
+function allDigitsOdd(num){
+	while(num!= 0){
+		c=num%10;
+		if (c%2 ==0) return false;
+		num =parseInt(num/10);
+	}
+	return true;
+}
+
+let a =parseInt(prompt("enter num",123));
+console.log(allDigitsOdd(a));
+*/
+
+/*
+function task5(mas){
+	for(let i=1;i<mas.length;i++){
+		if(mas[i]==mas[i-1])
+			return true;
+	}
+	return false;
+}
+
+let mas=[];
+for(let i=0; i<10;i++){
+	let x = Math.floor(Math.random()*11);
+	mas.push(x);
+}
+if(task5(mas)){
+	console.log(mas);
+	console.log("true");
+}
+else{
+	console.log(mas);
+	console.log("false");
+}
+*/
+
+/*function task6(a,b){
+	return a==b;
+}
+let a=4;
+let b=5;
+if(task6(a,b)){
+	console.log("true");
+}
+else{
+	console.log("false");
+}
+*/
+
+
+
+
